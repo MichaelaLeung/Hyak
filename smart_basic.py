29ea9f3375c2646a386982485521533777abfe5d
Michaela Leung <mwl@sauria.com>
2018-09-18 11:05:46 -0700

    smart test

--- smart_basic.py
+++ smart_basic.py
@@ -17,7 +17,7 @@ def smart_basic(res, lamin, lamax):
     earth_infile = "earth_avg.pt"
     
     HERE = os.path.dirname(os.path.abspath(__file__))
-    place = os.path.join(HERE, "smart_basic")
+    place = os.path.join(HERE, "smart_basic2")
 
     try:
         os.mkdir(place)
@@ -76,10 +76,69 @@ def smart_basic(res, lamin, lamax):
         mpl.rc('text', usetex=False)
         plt.switch_backend('agg')
 
+    n_phase = 1000
+    phases = np.linspace(0,2*np.pi,n_phase)
+    inclination = np.pi/2
+    phi_90 = np.pi/2
+    sma = 7500000
+    c = 299792.458
+    
+    fluxes = np.outer(flux2, np.ones(n_phase)) 
+    temp = np.arccos(-np.sin(inclination)*np.cos(phases))
+    phase_function = ((np.sin(temp)+(np.pi-temp)*(np.cos(temp)))/(np.pi)) 
+
+    fluxes2 = fluxes*(phase_function/phi_90)
+
+    rv_orb = 35.02* np.sin(inclination) *np.sin(phases)
+    rv_sys = -21.7 * np.ones_like(phases)
+    rv_bary = (29.8 * np.sin((11.2/365.)*phases))
+    rv = rv_sys + rv_orb - rv_bary
+
+    obs_wl = np.outer(wl,(1+rv/c))
+    f.write("plot time" + "\n")
+
+    # Create figure
     fig, ax = plt.subplots(figsize=(12,10))
-    ax.plot(wl,flux)
-    ax.plot(earth_wl, earth_flux)
-    fig.savefig(str(lamin) + ".png")
+    ax.set_ylabel("Phase Angle")
+
+    # Create a continuous norm to map from flux to colors
+    norm = plt.Normalize(np.min(fluxes2), np.max(fluxes2))
+
+    # Loop over phases
+    for i in range(len(phases)):
+
+        # Set dimensions
+        x = obs_wl[:,i]
+        y = phases[i] * np.ones_like(x)
+        z = fluxes2[:,i]
+
+        # Define line points and segments
+        points = np.array([x, y]).T.reshape(-1, 1, 2)
+        segments = np.concatenate([points[:-1], points[1:]], axis=1)
+
+        # Use linecollections to make color lines
+        lc = LineCollection(segments, cmap='bone', norm=norm)
+    
+        # Set the values used for colormapping
+        lc.set_array(z)
+        lc.set_linewidth(2)
+        line = ax.add_collection(lc)
+    
+    # Set the axis ranges
+    ax.set_ylim(min(phases), max(phases))
+
+    # Create colorbar
+    cbar = fig.colorbar(line)
+    cbar.set_label(r"Flux [W/m$^2$/$\mu$m]", rotation = 270, labelpad = 25)
+
+    ax2 = ax.twinx()
+    ax2.plot(earth_wl, earth_flux, 'r')
+    ax2.set_xlabel(r"Wavelength [$\mu$]")
+
+    fig_name = str(lamin) + "to" + str(lamax)
+    f.write(str(fig_name) + "\n")
+    fig.savefig(fig_name +  ".png")
+    f.write(str(fig_name) + "\n")
     
  
 
