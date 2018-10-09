bee757659e2f3bb7f28b93db27c2ef8e4f69640f
Michaela Leung <mwl@sauria.com>
2018-10-08 21:31:15 -0700

    fixed error

--- metrics_combined.py
+++ metrics_combined.py
@@ -127,7 +127,7 @@ def high_pass(flux, flux_low):
     long_flux = []
     for i in flux_low:
         j = 0
-        while j < 100: 
+        while j < int(len(flux)/len(flux_low)+1): 
             long_flux.append(i)
             j = j+1
             
@@ -135,9 +135,7 @@ def high_pass(flux, flux_low):
         temp = (flux[k] + long_flux[k]) / 2
         mixed.append(temp)
         k = k+1
-    
-    print(len(mixed))
-       
+           
     while z < len(mixed): 
         diff = abs(mixed[z] - long_flux[z])
         out.append(diff)
@@ -176,7 +174,7 @@ def integration(wl, flux, adj_flux, wl_low, flux_low, earth_wl, earth_flux):
     wl = wl[:len(out)]
     return(wl, out, abs(integrate.trapz(wl, out)))
 
-def outputs(lamin, lamax):
+def outputs(lamin, lamax, on):
     wl, flux, adj_flux, wl_low, flux_low, earth_wl, earth_flux = run_smart(lamin, lamax) #wl, flux, adj_flux, wl_low, flux_low, earth_wl, earth_flux
     adds = integration(wl, flux, adj_flux, wl_low, flux_low, earth_wl, earth_flux)
     adds2 = adds[2]
@@ -187,89 +185,95 @@ def outputs(lamin, lamax):
     out = label, "fpfs", np.median(adj_flux), "line cutoff", high, "integral", adds2, "together", (np.median(adj_flux)*high*adds2)
     f = open("test.txt", "a")
     f.write(str(out) + "\n")
+    if on > 0: 
+        n_phase = 1000
+        phases = np.linspace(0,2*np.pi,n_phase)
+        inclination = np.pi/2
+        phi_90 = np.pi/2
+        sma = 7500000
+        c = 299792.458
+        
+        fluxes = np.outer(adj_flux, np.ones(n_phase)) 
+        temp = np.arccos(-np.sin(inclination)*np.cos(phases))
+        phase_function = ((np.sin(temp)+(np.pi-temp)*(np.cos(temp)))/(np.pi)) 
 
-    n_phase = 1000
-    phases = np.linspace(0,2*np.pi,n_phase)
-    inclination = np.pi/2
-    phi_90 = np.pi/2
-    sma = 7500000
-    c = 299792.458
-    
-    fluxes = np.outer(adj_flux, np.ones(n_phase)) 
-    temp = np.arccos(-np.sin(inclination)*np.cos(phases))
-    phase_function = ((np.sin(temp)+(np.pi-temp)*(np.cos(temp)))/(np.pi)) 
+        fluxes2 = fluxes*(phase_function/phi_90)
 
-    fluxes2 = fluxes*(phase_function/phi_90)
+        rv_orb = 35.02* np.sin(inclination) *np.sin(phases)
+        rv_sys = -21.7 * np.ones_like(phases)
+        rv_bary = (29.8 * np.sin((11.2/365.)*phases))
+        rv = rv_sys + rv_orb - rv_bary
 
-    rv_orb = 35.02* np.sin(inclination) *np.sin(phases)
-    rv_sys = -21.7 * np.ones_like(phases)
-    rv_bary = (29.8 * np.sin((11.2/365.)*phases))
-    rv = rv_sys + rv_orb - rv_bary
+        obs_wl = np.outer(wl,(1+rv/c))
 
-    obs_wl = np.outer(wl,(1+rv/c))
+        import platform
+        if platform.system() == 'Jarvis':
+            # On a Mac: usetex ok
+            mpl.rc('font',**{'family':'serif','serif':['Computer Modern']})
+            mpl.rcParams['font.size'] = 25.0
+            mpl.rc('text', usetex=True)
+        elif platform.node().startswith("n"):
+            # On hyak: usetex not ok, must change backend to 'agg'
+            mpl.rc('font',**{'family':'serif','serif':['Computer Modern']})
+            mpl.rcParams['font.size'] = 25.0
+            mpl.rc('text', usetex=False)
+            plt.switch_backend('agg')
+        # Create figure
+        fig, ax = plt.subplots(3,1, figsize=(30,24))
+        ax[0].set_ylabel("Phase Angle")
 
-    import platform
-    if platform.system() == 'Jarvis':
-        # On a Mac: usetex ok
-        mpl.rc('font',**{'family':'serif','serif':['Computer Modern']})
-        mpl.rcParams['font.size'] = 25.0
-        mpl.rc('text', usetex=True)
-    elif platform.node().startswith("n"):
-        # On hyak: usetex not ok, must change backend to 'agg'
-        mpl.rc('font',**{'family':'serif','serif':['Computer Modern']})
-        mpl.rcParams['font.size'] = 25.0
-        mpl.rc('text', usetex=False)
-        plt.switch_backend('agg')
-    # Create figure
-    fig, ax = plt.subplots(3,1, figsize=(30,24))
-    ax[0].set_ylabel("Phase Angle")
+        # Create a continuous norm to map from flux to colors
+        norm = plt.Normalize(np.min(fluxes2), np.max(fluxes2))
 
-    # Create a continuous norm to map from flux to colors
-    norm = plt.Normalize(np.min(fluxes2), np.max(fluxes2))
+        # Loop over phases
+        for i in range(len(phases)):
 
-    # Loop over phases
-    for i in range(len(phases)):
+            # Set dimensions
+            x = obs_wl[:,i]
+            y = phases[i] * np.ones_like(x)
+            z = fluxes2[:,i]
 
-        # Set dimensions
-        x = obs_wl[:,i]
-        y = phases[i] * np.ones_like(x)
-        z = fluxes2[:,i]
+            # Define line points and segments
+            points = np.array([x, y]).T.reshape(-1, 1, 2)
+            segments = np.concatenate([points[:-1], points[1:]], axis=1)
 
-        # Define line points and segments
-        points = np.array([x, y]).T.reshape(-1, 1, 2)
-        segments = np.concatenate([points[:-1], points[1:]], axis=1)
+            # Use linecollections to make color lines
+            cmap = mpl.colors.ListedColormap(['white','white',
+                                      'black', 'black', "yellow",])
+            cmap.set_over('red')
+            cmap.set_under('blue')
 
-        # Use linecollections to make color lines
-        cmap = mpl.colors.ListedColormap(['white','white',
-                                  'black', 'black', "yellow",])
-        cmap.set_over('red')
-        cmap.set_under('blue')
+            lc = LineCollection(segments, cmap=cmap, norm=norm)
+        
+            # Set the values used for colormapping
+            lc.set_array(z)
+            lc.set_linewidth(2)
+            line = ax[0].add_collection(lc)
+        
+        # Set the axis ranges
+        ax[0].set_ylim(min(phases), max(phases))
+        ax[0].set_xlim(lamin, lamax)
+        
 
-        lc = LineCollection(segments, cmap=cmap, norm=norm)
-    
-        # Set the values used for colormapping
-        lc.set_array(z)
-        lc.set_linewidth(2)
-        line = ax[0].add_collection(lc)
-    
-    # Set the axis ranges
-    ax[0].set_ylim(min(phases), max(phases))
-    ax[0].set_xlim(lamin, lamax)
-    
+        # Create colorbar
+     
+        cbar = fig.colorbar(line, pad = 0.05, ax = ax[0], extend = 'both')
+        cbar.set_label(r"Reflectance", rotation = 270, labelpad = 25)
 
-    # Create colorbar
- 
-    cbar = fig.colorbar(line, pad = 0.05, ax = ax[0], extend = 'both')
-    cbar.set_label(r"Reflectance", rotation = 270, labelpad = 25)
+        ax[1].plot(wl, adj_flux, 'r')
+        ax[1].set_xlabel(r"Wavelength [$\mu$]")
+        ax[2].plot(wl2, out2, 'b')
+        ax[2].set_xlabel(r"Wavelength [$\mu$]")
 
-    ax[1].plot(wl, adj_flux, 'r')
-    ax[1].set_xlabel(r"Wavelength [$\mu$]")
-    ax[2].plot(wl2, out2, 'b')
-    ax[2].set_xlabel(r"Wavelength [$\mu$]")
+        fig_name = str(lamin) + "to" + str(lamax)
+        fig.savefig("plots/" + fig_name +  ".png")
+    else:
+        fig, ax = plt.subplots(2,1, figsize=(30,24))
+        ax[0].plot(wl, adj_flux, 'r')
+        ax[0].set_xlabel(r"Wavelength [$\mu$]")
+        ax[1].plot(wl2, out2, 'b')
+        ax[1].set_xlabel(r"Wavelength [$\mu$]")
 
-    fig_name = str(lamin) + "to" + str(lamax)
-    fig.savefig("plots/" + fig_name +  ".png") 
-    
 
 if __name__ == '__main__':
 
@@ -296,11 +300,13 @@ if __name__ == '__main__':
         for i in number:
             i = float(i)
             i = i/100
-            outputs(i, i+0.02)
+            outputs(i, i+0.02, 0)
     else:
+        f = open("test.txt", "w")
         # Presumably, on a regular computer: ready to run
-       number = range(50,250, 10)
-       for i in number:
-            i = float(i)
-            i = i/100
-            outputs(i, i+0.01)
+ #      number = range(50,250, 10)
+ #      for i in number:
+ #           i = float(i)
+ #           i = i/100
+ #           outputs(i, i+0.01)
+        outputs(1.02,1.04, 0)
