046103693e5c3f8f937d07452627e1ba7aeb0d49
Michaela Leung <mwl@sauria.com>
2019-02-01 15:01:33 -0800

    new context plots for not-methane)

--- smart_plot.py
+++ smart_plot.py
@@ -134,12 +134,16 @@ if __name__ == '__main__':
  #       smart_basic(0.67, 0.71, "Oxygen B band (0.69)", 'wet')
  #       smart_basic(0.74, 0.78, "Oxygen A band (0.76)", 'wet')
  #       smart_basic(1.25,1.29, "1.27 band", 'wet')
-        smart_basic(1.63, 1.67, "Methane 1.65", 'dry')
-        smart_basic(2.28, 2.32, "Methane 2.3", 'dry')
-        smart_basic(3.28,3.32, "Methane 3.3", 'dry')
-        smart_basic(1.63, 1.67, "Methane 1.65", 'wet')
-        smart_basic(2.28, 2.32, "Methane 2.3", 'wet')
-        smart_basic(3.28,3.32, "Methane 3.3", 'wet')
+ #       smart_basic(1.63, 1.67, "Methane 1.65", 'dry')
+ #       smart_basic(2.28, 2.32, "Methane 2.3", 'dry')
+ #       smart_basic(3.28,3.32, "Methane 3.3", 'dry')
+ #       smart_basic(1.63, 1.67, "Methane 1.65", 'wet')
+ #       smart_basic(2.28, 2.32, "Methane 2.3", 'wet')
+ #       smart_basic(3.28,3.32, "Methane 3.3", 'wet')
+         smart_basic(1.6,1.75, "1.6-1.75 context", 'dry')
+         smart_basic(2,2.5, "2-2.5 context", 'dry')
+         smart_basic(3,3.5, "3-3.5 context", 'dry')
+
 
     else:
         # Presumably, on a regular computer: ready to run
