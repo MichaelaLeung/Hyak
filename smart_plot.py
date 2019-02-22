5695ba2a79f98ff1b7e8567c0a3eaa3ba1efd85f
Michaela Leung <mwl@sauria.com>
2019-02-21 16:01:05 -0800

    fixing indent error

--- smart_plot.py
+++ smart_plot.py
@@ -141,9 +141,9 @@ if __name__ == '__main__':
  #       smart_basic(2.28, 2.32, "Methane 2.3", 'wet')
  #       smart_basic(3.28,3.32, "Methane 3.3", 'wet')
  #        smart_basic(1.6,1.75, "1.6-1.75 context", 'dry')
-         smart_basic(2.2,2.5, "2-2.5 context", 'dry')
-         smart_basic(3.2,3.5, "3-3.5 context", 'dry')
-
+ #        smart_basic(2.2,2.5, "2-2.5 context", 'dry')
+ #        smart_basic(3.2,3.5, "3-3.5 context", 'dry')
+#
 
     else:
         # Presumably, on a regular computer: ready to run
