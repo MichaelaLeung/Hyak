9ea583d13ae2317b7c0976fdf9bf60df591f1133
Michaela Leung <mwl@sauria.com>
2019-04-05 17:03:33 -0700

    changing indexing error in o2o2 plots

--- testplot_noO4.py
+++ testplot_noO4.py
@@ -228,10 +228,10 @@ if __name__ == '__main__':
                                rm_after_submit = True)
     elif platform.node().startswith("n"):
         # On a mox compute node: ready to run
-        plotting(0.61,0.65,0,"Gamma band (0.63) Ocean Loss")
-        plotting(0.67,0.71,0,"Oxygen B band (0.69) Ocean Loss")
-        plotting(0.74,0.78,0,"Oxygen A band (0.76) Ocean Loss")
-        plotting(1.25,1.29,0,"1.27 Ocean Loss")
+ #       plotting(0.61,0.65,0,"Gamma band (0.63) Ocean Loss")
+ #       plotting(0.67,0.71,0,"Oxygen B band (0.69) Ocean Loss")
+ #       plotting(0.74,0.78,0,"Oxygen A band (0.76) Ocean Loss")
+ #       plotting(1.25,1.29,0,"1.27 Ocean Loss")
         plotting(0.61,0.65,1,"Gamma band (0.63) Ocean Outgassing")
         plotting(0.67,0.71,1,"Oxygen B band (0.69) Ocean Outgassing")
         plotting(0.74,0.78,1,"Oxygen A band (0.76) Ocean Outgassing")
