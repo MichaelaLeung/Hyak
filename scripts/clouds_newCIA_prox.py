a172df4cc60519352c0a287a12250b65efbe2979
Michaela Leung <mwl@sauria.com>
2019-08-23 12:33:53 -0700

    adding paths for clouds

--- scripts/clouds_newCIA_prox.py
+++ scripts/clouds_newCIA_prox.py
@@ -14,8 +14,8 @@ def clouds(res, cirrus, strato):
     lamax = 2.0
     
     sim = smart.interface.Smart(tag = "prox")
-    sim.smartin.alb_file = "composite1_txt.txt"
-    infile = "profile_Earth_proxb_.pt_filtered"
+    sim.smartin.alb_file = "/gscratch/vsm/mwjl/projects/high_res/inputs/composite1_txt.txt"
+    infile = "/gscratch/vsm/mwjl/projects/high_res/inputs/profile_Earth_proxb_.pt_filtered"
 
     HERE = os.path.dirname(os.path.abspath(__file__))
     place = os.path.join(HERE, "clouds")
@@ -36,7 +36,7 @@ def clouds(res, cirrus, strato):
 
     sim.load_atmosphere_from_pt(infile, addn2 = False)
     o2 = sim.atmosphere.gases[6]
-    o2.cia_file = "cia_adj_mix.cia"
+    o2.cia_file = "/gscratch/vsm/mwjl/projects/high_res/inputs/o4_calc.cia"
 
     sim.smartin.FWHM = res
     sim.smartin.sample_res = res
@@ -70,9 +70,11 @@ def clouds(res, cirrus, strato):
     flux = sim.output.rad.pflux
     sflux = sim.output.rad.sflux
 
-    adj_flux = flux/sflux
+    adj_flux = flux/sflux * math.pi
+    r_km = 149,598,000 * sim.smartin.r_AU
+    fpfs = flux/sflux * (sim.smartin.radius/r_km)**2
 
-    return(wl, adj_flux)
+    return(wl, adj_flux, fpfs)
 
 def longplot():
     HERE = os.path.dirname(os.path.abspath(__file__))
@@ -89,9 +91,9 @@ def longplot():
     res = 0.01
     
     sim = smart.interface.Smart(tag = "prox")
-    infile = "profile_Earth_proxb_.pt_filtered"
+    infile = "/gscratch/vsm/mwjl/projects/high_res/inputs/profile_Earth_proxb_.pt_filtered"
     label = "Prox"
-    sim.smartin.alb_file = "composite1_txt.txt"
+    sim.smartin.alb_file = "/gscratch/vsm/mwjl/projects/high_res/inputs/composite1_txt.txt"
     sim.set_run_in_place(place) 
     sim.set_executables_automatically()
 
@@ -102,7 +104,7 @@ def longplot():
 
     sim.load_atmosphere_from_pt(infile, addn2 = False)
     o2 = sim.atmosphere.gases[6]
-    o2.cia_file = "cia_adj_mix.cia"
+    o2.cia_file = "/gscratch/vsm/mwjl/projects/high_res/inputs/o4_calc.cia"
 
     sim.smartin.sza = 57
 
@@ -126,23 +128,30 @@ def longplot():
     flux = sim.output.rad.pflux
     sflux = sim.output.rad.sflux
 
-    adj_flux = flux/sflux
-    
-    return(wl, adj_flux)
+    adj_flux = flux/sflux * math.pi
+    r_km = 149,598,000 * sim.smartin.r_AU
+    fpfs = flux/sflux * (sim.smartin.radius/r_km)**2
 
+    return(wl, adj_flux, fpfs)
+    
 
 def plotting():
-    cirrus_wl, cirrus_flux = clouds(0.01, 1, 0)
-    strato_wl, strato_flux = clouds(0.01, 0, 1)
-    wl, flux = longplot()
+    cirrus_wl, cirrus_flux, cirrus_fpfs = clouds(0.01, 1, 0)
+    strato_wl, strato_flux, strato_fpfs = clouds(0.01, 0, 1)
+    wl, flux,fpfs = longplot()
     length_wl = min(len(cirrus_wl), len(strato_wl), len(wl))-1
     avg_wl = (cirrus_wl[:length_wl] + strato_wl[:length_wl] + wl[:length_wl])/3
     avg_flux = (cirrus_flux[:length_wl] + strato_flux[:length_wl] +flux[:length_wl])/3
+    avg_fpfs = (cirrus_fpfs[:length_wl] + strato_fpfs[:length_wl] +fpfs[:length_wl])/3
+
     fig, ax = plt.subplots(figsize = (30, 10))
-    ax3 = ax.twinx()
-    ax3.plot(avg_wl, avg_flux)
-    ax3.set_ylabel("Reflectance")
+    ax.plot(avg_wl, avg_flux)
+    ax.set_ylabel("Reflectance")
     ax.set_xlabel("Wavelength ($\mu$ m)")
+
+    ax2 = ax.twinx()
+    ax2.set_ylabel("Planet-to-star contrast ratio")
+    ax2.plot(avg_wl, avg_fpfs)
     ax.set_title("Simulated Earth-like planet with clouds orbiting Proxima Centauri")
     ax.set_xlim(0.5,2)
     ax.axvspan(0.61, 0.65, alpha=0.5, color='0.85')
@@ -150,7 +159,7 @@ def plotting():
     ax.axvspan(0.74, 0.78, alpha=0.5, color='0.85')
     ax.axvspan(1.25, 1.29, alpha=0.5, color='0.85')
     ax.plot(avg_wl, avg_flux)
-    fig.savefig("avg_clouds.png", bbox_inches = 'tight')
+    fig.savefig("/gscratch/vsm/mwjl/projects/plots/avg_clouds.png", bbox_inches = 'tight')
     
 if __name__ == '__main__':
 
