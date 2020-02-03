b6823ff718636db830f0576c850283500e65f204
Michaela Leung <mwl@sauria.com>
2020-02-02 18:17:07 -0800

    adding mask for fractoins

--- scripts/clouds_2020.py
+++ scripts/clouds_2020.py
@@ -664,8 +664,9 @@ def plotting(lamin, lamax, title):
     wl, flux = earth_like_hyak(lamin, lamax)
     wl2, flux2 = cirrus(lamin, lamax)
     wl3, flux3 = strato(lamin, lamax)
-    print(len(flux), len(flux2), len(flux3))
-    avg_flux = (0.5*flux+0.25*flux2+0.25*flux3)
+    m, m_clouds = get_common_masks(wl, wl2)
+    print(len(flux[m]), len(flux2[m_clouds]), len(flux3[m_clouds]))
+    avg_flux = (0.5*flux[m]+0.25*flux2[m_clouds]+0.25*flux3[m_clouds])
     
     ocean_wl, ocean_flux = ocean_outgassing(lamin, lamax)
     ocean_wl2, ocean_flux2 = outgassing_cirrus(lamin, lamax)
